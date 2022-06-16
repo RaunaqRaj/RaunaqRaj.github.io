204bbaf216b4e012b7ba766b7bde2752b32356e1
RaunaqRaj <raunaqraj2003@gmail.com>
2022-06-17 00:55:09 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php include 'components/head_start.php'?>
+
+</head>
+
+<body>
+<?php include 'components/preloader.php' ?>
+
+<!-- navbar -->
+  <?php include 'components/header.php'?>
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://images.indianexpress.com/2021/06/Welcomhotel-Tavleen-Chail_1200.jpg" class="d-block"
+          alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 data-animation="fade-In-Down" style="animation-delay: 100ms;">Welcome To Paradise Hotels</h5>
+          <p>
+            Lavish social and business events
+          </p>
+          <div class="slider-btn">
+          <?php include 'components/book.php'?>
+          </div>
+        </div>
+
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.princehotels.com/wp-content/uploads/2019/04/aboutslider2-1.jpg" class="d-block"
+          alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>A moment of pure prestige</h5>
+          <p>
+            A moment of pure prestige
+          </p>
+          <div class="slider-btn">
+          <?php include 'components/book.php'?>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="http://cdn.cnn.com/cnnnext/dam/assets/160726150143-us-beautiful-hotels-17-bellagio-2.jpg"
+          class="d-block" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>
+            The privacy and individuality of a custom
+          </h5>
+          <p>
+            The Residences have their own dedicated private entrance as well </p>
+          <div class="slider-btn">
+          <?php include 'components/book.php'?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5 mx-2 hi">
+
+<div class="col">
+  <div class="card card-cover fine hello h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
+    style="background-image: url('https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs='); background-size: 500px;">
+    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+      <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold all text-center">Classic Room</h2>
+      <ul class="d-flex list-unstyled mt-auto">
+        <li class="me-auto">
+        </li>
+        <li class="d-flex align-items-center me-3">
+
+        </li>
+        <li class="d-flex align-items-center">
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card card-cover fine2 hello h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
+    style="background-image: url('https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80'); background-size: 490px;">
+    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+      <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold all text-center">Presidential Suite</h2>
+      <ul class="d-flex list-unstyled mt-auto">
+        <li class="me-auto">
+        </li>
+        <li class="d-flex align-items-center me-3">
+
+        </li>
+        <li class="d-flex align-items-center">
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="col">
+  <div class="card card-cover fine3 hello h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg mx-2"
+    style="background-image: url('https://images.indianexpress.com/2021/06/Welcomhotel-Tavleen-Chail_1200.jpg'); background-size: 500px;">
+    <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+      <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold all text-center" style="font-size: 35px;top:180px;">Luxury
+        appartment</h2>
+    </div>
+  </div>
+</div>
+</div>
+
+<section class="testimonials">
+    <div class="container">
+      <h1><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
+  <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+</svg>Room Category<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
+  <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"/>
+</svg></h1>
+      <p class="text-center">Every detail about the room is here
+      </p>
+      <div class="row" id="categories">
+          
+          </div> 
+        </div>
+      </div>
+    </section>
+
+  <?php include 'components/footer.php' ?>
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+<?php include 'components/script_start.php'?>
+<script src="assests/js/room_category.js"></script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
